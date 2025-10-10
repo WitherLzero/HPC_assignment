f89584bbfa1ef4936ca843ed263995b4567d816e
Samuka007 <24754678@student.uwa.edu.au>
2025-10-10 23:50:18 +0800

    update readme

---
+++ final-mpi/include/core_mpi.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "stdbool.h"
+
+extern int RANK;
+extern int SIZE;
+
+void init_mpi(int* argc, char*** argv);
+inline bool is_root() { return RANK == 0; }
+
+#define ROOT_DO(block) do { if (is_root()) { block; } } while (0);
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define DEBUGF(...) \
+    ROOT_DO( \
+        if (VERBOSE) { \
+            printf(ANSI_COLOR_BLUE "[DEBUG] " ANSI_COLOR_RESET); \
+            printf(__VA_ARGS__); \
+            printf("\n"); \
+        } \
+    )
+
+#define ERRORF(...) \
+    ROOT_DO( \
+        fprintf(stderr, ANSI_COLOR_RED "[ERROR] " ANSI_COLOR_RESET); \
+        fprintf(stderr, __VA_ARGS__); \
+        fprintf(stderr, "\n"); \
+    )
+
+#define INFOF(...) \
+    ROOT_DO(\
+        printf(ANSI_COLOR_GREEN "[INFO] " ANSI_COLOR_RESET); \
+        printf(__VA_ARGS__); \
+        printf("\n"); \
+    )
